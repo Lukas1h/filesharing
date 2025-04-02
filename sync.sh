5ec78c1219987279d03c297c5330651214294f73
Lukas Hahn <lukas1h07@gmail.com>
2025-04-02 10:12:17 -0700

    Auto commit: Wed Apr  2 10:12:17 PDT 2025

--- sync.sh
+++ sync.sh
@@ -3,7 +3,8 @@
 REPO_PATH="/Users/blackout/files.lukashahn.art"
 cd $REPO_PATH
 
-fswatch -o . | while read change; do
+fswatch -o . \
+        --exclude ".git" | while read change; do
     echo "Changes detected, syncing..."
     git add .
     git commit -m "Auto commit: $(date)"
