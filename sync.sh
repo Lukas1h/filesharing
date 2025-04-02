d837627762e95ff6953f4cd6ac4488656d710192
Lukas Hahn <lukas1h07@gmail.com>
2025-04-02 10:10:05 -0700

    Auto commit: Wed Apr  2 10:10:05 PDT 2025

---
+++ sync.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+REPO_PATH="/Users/blackout/files.lukashahn.art"
+cd $REPO_PATH
+
+fswatch -o . | while read change; do
+    echo "Changes detected, syncing..."
+    git add .
+    git commit -m "Auto commit: $(date)"
+    git push origin main  # Change "main" if your branch name is different
+done
